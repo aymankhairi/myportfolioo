4f336ac425104abb66268f2586418b085b6c11c1
Ayman <aymanakh99@gmail.com>
2026-07-21 01:08:09 +0300

    Update portfolio

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -272,17 +272,19 @@ export default function About() {
                     {stat.label}
                   </p>
                 </div>
-                <div className="flex flex-wrap gap-3 mt-8">
-                  {[
-                    "Web Applications",
-                    "E-Commerce",
-                    "Business Systems",
-                    "Mobile Apps",
-                    "Custom Software",
-                  ].map((item) => (
-                    <span
-                      key={item}
-                      className="
+              </motion.div>
+            ))}
+            <div className="flex flex-wrap gap-3 mt-8">
+              {[
+                "Web Applications",
+                "E-Commerce",
+                "Business Systems",
+                "Mobile Apps",
+                "Custom Software",
+              ].map((item) => (
+                <span
+                  key={item}
+                  className="
         px-4
         py-2
         rounded-full
@@ -290,13 +292,11 @@ export default function About() {
         border-white/10
         bg-white/5
       "
-                    >
-                      {item}
-                    </span>
-                  ))}
-                </div>
-              </motion.div>
-            ))}
+                >
+                  {item}
+                </span>
+              ))}
+            </div>
           </motion.div>
         </div>
 
